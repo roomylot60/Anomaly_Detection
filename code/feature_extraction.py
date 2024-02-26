d6d5eb54f1811e9d4a5018b54b3e95fe0ceb94a2
Patrick <min2max.dev@gmail.com>
2024-02-26 18:35:33 +0900

    distribute feature_extraction modules

---
+++ code/feature_extraction.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+
+class FeatureExtractor(nn.Module):
+    def __init__(self, model):
+        super(FeatureExtractor, self).__init__()
+        # 마지막 fc 레이어를 제외한 모든 레이어를 가져옵니다.
+        self.features = nn.Sequential(*list(model.children())[:-1])
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        return x
+
+
+    
+class Autoencoder(nn.Module):
+    def __init__(self, input_dim, encoding_dim):
+        super(Autoencoder, self).__init__()
+        # 인코더 정의
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, 1024),
+            nn.ReLU(True),
+            nn.Linear(1024, 512),
+            nn.ReLU(True),
+            nn.Linear(512, encoding_dim),
+            nn.ReLU(True)
+        )
+        # 디코더 정의
+        self.decoder = nn.Sequential(
+            nn.Linear(encoding_dim, 512),
+            nn.ReLU(True),
+            nn.Linear(512, 1024),
+            nn.ReLU(True),
+            nn.Linear(1024, input_dim),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
